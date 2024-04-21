ddcd0b35a3c152d23c4df9085113227360adf0a6
TayKaunda <165219200+TayKaunda@users.noreply.github.com>
2024-04-21 20:19:57 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+    },
+    navigation: {
+        nextE1:".swiper-button-next",
+        prevE1:".swiper-button-prev",
+    },
+}):
